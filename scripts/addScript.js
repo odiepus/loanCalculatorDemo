17596be9deabfcf128574710dc46bf8706254f56
Hector Herrera <odiepus@gmail.com>
2019-01-31 18:44:35 -0600

    added logic for addition page and another level on the page to show the user input. Next I will add a next button that will refresh page and clear the input field.

---
+++ scripts/addScript.js
@@ -0,0 +1,31 @@
+var x = Math.floor(Math.random() * 10);
+var y = Math.floor(Math.random() * 10);
+
+window.onload = load;
+
+function load(){
+  document.getElementById("op1").innerHTML = x;
+  document.getElementById("op2").innerHTML = y;
+}
+
+
+
+document.getElementById("submission").addEventListener("click", addFunc);
+
+function addFunc(e){
+  var sum = x + y;
+  var userAnswer = document.getElementById("answer").value;
+
+  console.log(userAnswer);
+
+  document.getElementById("userAnswer").innerHTML = userAnswer;
+  
+  if (sum == userAnswer){
+    document.getElementById("correctAnswer").innerHTML = sum + " Correct!!!";
+  }
+  else{
+    document.getElementById("correctAnswer").innerHTML = sum + " Incorrect.";
+  }
+
+  e.preventDefault();
+}
